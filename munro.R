2388ee0bbac3c9cb86da1154bbd35e2ca6d76290
markocherrie <mark.cherrie.mc@gmail.com>
2017-06-29 15:36:39 +0100

    first commit

---
+++ munro.R
@@ -0,0 +1,11 @@
+# scrape locations of munros
+
+# take points equidistant from location of munro, east to west?
+
+# Create polyline of points
+
+# upload DEM model
+
+# extract values of DEM along polyline
+
+# plot result
