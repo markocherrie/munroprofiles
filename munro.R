8688cf326677cc764f7b8906417ef971648a48fa
markocherrie <mark.cherrie.mc@gmail.com>
2017-06-29 15:49:32 +0100

    Adding data

--- munro.R
+++ munro.R
@@ -1,6 +1,14 @@
 # scrape locations of munros
 
+# actually just downloaded from here : http://www.hills-database.co.uk/
+data<-read.csv("data/DoBIH_v15.5.csv")
+munros<-data[data$M==1,]
+
 # take points equidistant from location of munro, east to west?
+# let's do one
+munro1<-munros[1,]
+
+
 
 # Create polyline of points
 
